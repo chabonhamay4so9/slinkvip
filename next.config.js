6bd9aa1924b83606523350300a2548bc1d8122ea
chabonhamay4so9 <93881933+chabonhamay4so9@users.noreply.github.com>
2023-11-26 21:40:52 +0700

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  env: {
+    url:"https://vnexpress.net/chu-tich-nuoc-den-tokyo-bat-dau-tham-chinh-thuc-nhat-ban-4681507.html"
+  },
+}
+
+module.exports = nextConfig
